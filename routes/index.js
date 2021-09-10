0bd46e07718f7a4cedf288306ad768fb0da42f82
Gautam Vaja <83920497+gautamVaja@users.noreply.github.com>
2021-09-10 12:12:08 +0530

    Ready with the full template to make the project

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const path = require("path");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Gautam Express" });
 });
+
+/* GET React App */
+// router.get("/app", function (req, res, next) {
+//   res.sendFile(
+//     path.join(__dirname, "../express-react/react-spa/public", "app.html")
+//   );
+// });
+
+/* GET settings page */
+// router.get("/settings", function (req, res, next) {
+//   res.render("settings");
+// });
+
+// C:\Users\admin\Desktop\Major media app\express-react\react-spa\public
+// __dirname = C:\Users\admin\Desktop\Major-media-app\routes\
 
 module.exports = router;
